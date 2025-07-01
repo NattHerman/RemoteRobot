655b400e1a9566ecfbdad0b918d671c0bc819362
NattHerman <daghermanoyerhamn@gmail.com>
2025-07-01 21:14:25 +0200

    Update serialTest.py

--- serialTest.py
+++ serialTest.py
@@ -1,20 +1,15 @@
 from serial import Serial
 from time import sleep
 
-print("Testing '/dev/ttyS0'")
-port = Serial("/dev/ttyS0", baudrate=9600)
+def test(serial_path):
+    print("Testing", serial_path)
+    port = Serial(serial_path, baudrate=9600)
 
-for i in range(25):
-    b = [255, 0, i * 10]
-    port.write(bytes(b))
-    print(f"Wrote bytes: {b}")
-    sleep(0.1)
+    for i in range(25):
+        b = bytes([255, 0, i * 10])
+        port.write(b)
+        print(f"Wrote bytes: {b}")
+        sleep(0.1)
 
-print("Testing '/dev/serial0'")
-port = Serial("/dev/serial0", baudrate=9600)
-
-for i in range(25):
-    b = [255, 0, i * 10]
-    port.write(bytes(b))
-    print(f"Wrote bytes: {b}")
-    sleep(0.1)
+test("/dev/ttyS0")
+test("/dev/serial0")

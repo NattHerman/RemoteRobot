41847c4c01aea7bc3ab1f27e689f251ed08f978b
NattHerman <daghermanoyerhamn@gmail.com>
2025-07-02 19:19:44 +0200

    used wrong representation of 13

--- serialTest.py
+++ serialTest.py
@@ -5,7 +5,7 @@ def test(serial_path):
     print("Testing", serial_path)
     port = Serial(serial_path, baudrate=115_200)
 
-    port.write(b"#0 P1500 S750 \x13")
+    port.write(b"#0 P1500 S750 \x0D")
 
 test("/dev/ttyS0")
 #test("/dev/serial0")

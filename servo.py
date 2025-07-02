c7db16c7ec23f42e8afa4d522a8d805293c9a5e4
NattHerman <daghermanoyerhamn@gmail.com>
2025-07-02 20:21:19 +0200

    Update servo.py to modified SSC32 servo

--- servo.py
+++ servo.py
@@ -1,12 +1,24 @@
 from serial import Serial
 
 
-class MiniSSCIIServo():
-    def __init__(self, servo_index: int, serial_port: Serial):
+class SSC32ModifiedServo():
+    def __init__(self, servo_index: int, SSC32_port: Serial):
         self._index = servo_index
-        self._port = serial_port
+        self._ser = SSC32_port
+
+        ## Offset where servo is still, in microseconds.
+        self.offset = 0
+
+    def set_speed(self, speed: int, ):
+        position = 1500 + speed + self.offset
+        string = f"#{self._index} P{position} \r"
+        self._ser.write(string.encode("ascii"))
 
 
-    ## speed must be an int in the range [-127, 127] (inclusive).
-    def set_speed(self, speed: int):
-        pass
+if __name__ == "__main__":
+    SSC32 = Serial("/dev/ttyS0", 115_200)
+
+    servo_wheel = SSC32ModifiedServo(0, SSC32)
+
+    speed = int(input("Servo speed:"))
+    servo_wheel.set_speed(speed)

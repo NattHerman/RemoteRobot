fe3f542a9f556ae59eb5ac979178eac4fcb07a79
NattHerman <daghermanoyerhamn@gmail.com>
2025-07-02 19:03:00 +0200

    Changed servo control board to SSC-32

--- serialTest.py
+++ serialTest.py
@@ -3,13 +3,9 @@ from time import sleep
 
 def test(serial_path):
     print("Testing", serial_path)
-    port = Serial(serial_path, baudrate=9600)
+    port = Serial(serial_path, baudrate=115_200)
 
-    for i in range(25):
-        b = bytes([255, 0, i * 10])
-        port.write(b)
-        print(f"Wrote bytes: {b}")
-        sleep(0.1)
+    port.write(b"#0 P1500 S750 \x13")
 
 test("/dev/ttyS0")
-test("/dev/serial0")
+#test("/dev/serial0")

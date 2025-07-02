c82e603f0f8254a29fa4ada67af5a8f84b5fef51
NattHerman <daghermanoyerhamn@gmail.com>
2025-07-02 19:50:29 +0200

    Now we have control over the servo

--- serialTest.py
+++ serialTest.py
@@ -10,8 +10,9 @@ def test(serial_path):
     print("Testing", serial_path)
     port = Serial(serial_path, baudrate=115_200)
     
-    debug_write(port, b"#1 P1500 \r")
-    debug_write(port, b"#1 P1600 T2000 \r")
+    debug_write(port, b"#1 P1500 T100 \r")
+    sleep(0.1)
+    debug_write(port, b"#1 P700 T100 \r")
 
 test("/dev/ttyS0")
-test("/dev/serial0")
+#test("/dev/serial0")

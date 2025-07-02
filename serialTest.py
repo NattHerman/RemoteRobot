f4e8c949a6424d3053d1774ab6e6835908997198
NattHerman <daghermanoyerhamn@gmail.com>
2025-07-02 19:30:53 +0200

    update servoTest.py

--- serialTest.py
+++ serialTest.py
@@ -4,8 +4,10 @@ from time import sleep
 def test(serial_path):
     print("Testing", serial_path)
     port = Serial(serial_path, baudrate=115_200)
-
-    port.write(b"#0 P1500 S750 \x0D")
+    
+    b = "#0 P1500 S750 <cr>".encode("ascii")
+    port.write(b)
+    print(b)
 
 test("/dev/ttyS0")
-#test("/dev/serial0")
+test("/dev/serial0")

33a33872c35ba392a57a7a9dd88d036933a61b24
NattHerman <daghermanoyerhamn@gmail.com>
2025-07-10 19:09:54 +0200

    flask server now runs on a thread

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+import threading
 from flask import Flask, render_template
 
 app = Flask(__name__, template_folder="static", static_folder="static")
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+
 if __name__ == "__main__":
-    app.run("0.0.0.0", debug=True)
+    server_thread = threading.Thread(target=app.run, args=("0.0.0.0",), kwargs={"debug": False})
+
+    server_thread.start()

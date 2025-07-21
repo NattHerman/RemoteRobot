4e898bf17a75e47b9ddd9daa9265e836eec410c5
NattHerman <daghermanoyerhamn@gmail.com>
2025-07-21 20:58:32 +0200

    server receives input from client

--- main.py
+++ main.py
@@ -11,6 +11,18 @@ socketio = SocketIO(app)
 def index():
     return render_template("index.html")
 
+@socketio.event
+def robot_update(speed, turning):
+    print('received args:', speed, turning)
+
+@socketio.on('disconnect')
+def test_disconnect(reason):
+    ## Halt robot
+    pass
+
+@socketio.on('my event')
+def handle_my_custom_event(json):
+    print('received json: ' + str(json))
 
 if __name__ == "__main__":
     socketio.run(app, "0.0.0.0", debug=True)
--- static/script.js
+++ static/script.js
@@ -1,1 +1,60 @@
 console.log("hello world")
+
+var socket = io();
+
+// Clamped between -1 and 1
+var speed = 0
+// Clamped between -1 and 1
+var turning = 0
+
+addEventListener("keydown", (event) => {
+    //console.log("down", event.key, "repeating", event.repeat)
+    
+    if (event.repeat) {
+        return
+    }
+
+    switch(event.key) {
+        case "w":
+            speed += 1
+            break;
+        case "s":
+            speed -= 1
+            break;
+        case "a":
+            turning += 1
+            break;
+        case "d":
+            turning -= 1
+            break;
+    }
+
+    console.log(speed, turning)
+    socket.emit("robot_update", speed, turning)
+})
+
+addEventListener("keyup", (event) => {
+    //console.log("up", event.key)
+    
+    switch(event.key) {
+        case "w":
+            speed -= 1
+            break;
+        case "s":
+            speed += 1
+            break;
+        case "a":
+            turning -= 1
+            break;
+        case "d":
+            turning += 1
+            break;
+    }
+
+    console.log(speed, turning)
+    socket.emit("robot_update", speed, turning)
+})
+
+socket.on('connect', () => {
+    socket.emit('my event', {data: 'I\'m connected!'});
+});

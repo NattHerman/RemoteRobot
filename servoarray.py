0317024432290927269f4896ec6b258f40dba99c
NattHerman <daghermanoyerhamn@gmail.com>
2025-07-24 00:06:35 +0200

    rename length to conut
    Because this is a physical array of servos and not a conceptual array. There is a discrete number of servos in this array, we're not talking abut the concept of an array that can have any length.

--- servoarray.py
+++ servoarray.py
@@ -1,21 +1,26 @@
 from serial import Serial
 
-class ModifiedServoArray():
+
+class ModifiedServoArray(): ## TODO: Rename to ContinousServoArray?
+    """
+    Array of continous servos. The speed of each servo is modified by setting the corresponding value in the self.speeds list.
+    """
+
     def __init__(self, serial, indices = range(4)):
         self._indices = indices
         self._serial = serial
-        self.length = len(indices)
-        self.speeds = [0] * self.length
-        self.offsets = [0] * self.length
+        self.count = len(indices)
+        self.speeds = [0] * self.count
+        self.offsets = [0] * self.count
 
     def stop_all(self):
-        for i in self.length:
+        for i in self.count:
             self.speeds[i] = self.offsets[i]
     
     def update(self):
         # Create command to update all servos at once
         command = ""
-        for i in range(self.length):
+        for i in range(self.count):
             speed = self.speeds[i] + self.offsets[i]
             command += f"#{self._indices[i]} P{speed}"
         command += "\r"

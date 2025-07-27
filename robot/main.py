22848314d93a3ccc628f2cf0671ea7192b67d0d6
NattHerman <daghermanoyerhamn@gmail.com>
2025-07-27 20:46:38 +0200

    Set speed to zero when new offset is set

--- robot/main.py
+++ robot/main.py
@@ -80,6 +80,10 @@ def set_offsets(offsets):
     servo_array.offsets = offsets
     save_offsets(servo_array)
     emit("send_existing_offsets", servo_array.offsets, broadcast=True)
+    # Set speed to zero when new offset is set
+    if serial_available:
+        robot.set_normalized_speed(0)
+        robot.set_normalized_turning_rate(0)
 
 # When a client disconnects stop the robot.
 @socketio.on('disconnect')

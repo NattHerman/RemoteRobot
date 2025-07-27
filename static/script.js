11614c9600543f74b7467129cdb0388d3cca51e5
NattHerman <daghermanoyerhamn@gmail.com>
2025-07-27 17:34:16 +0200

    Stop robot when tab lost focus

--- static/script.js
+++ static/script.js
@@ -5,6 +5,14 @@ var speed = 0
 // Clamped between -1 and 1
 var turning = 0
 
+// Stop robot when tab lost focus
+window.addEventListener("blur", () => {
+    speed = 0
+    turning = 0
+    console.log("Lost focus, stopping robot")
+    socket.emit("robot_update", 0, 0)
+})
+
 addEventListener("keydown", (event) => {
     //console.log("down", event.key, "repeating", event.repeat)
     
